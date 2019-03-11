c9e73642a46ae8f6a7553e678b444e2b1811cbc0
Thomas Day <tday42@gmail.com>
2019-03-11 17:51:31 +0000

    Reverted to old rotation method

--- Assets/Scripts/Behaviours/ObjectManipulator.cs
+++ Assets/Scripts/Behaviours/ObjectManipulator.cs
@@ -5,7 +5,7 @@ using VRTK;
 
 public class ObjectManipulator : MonoBehaviour {
 
-    VRTK_ControllerEvents m_Controller;
+    private VRTK_ControllerEvents m_Controller;
 
     private Vector2 m_PrevVector;
     private float m_XVector, m_YVector;
@@ -24,65 +24,96 @@ public class ObjectManipulator : MonoBehaviour {
     {
         if (m_Controller.touchpadTouched)
         {
-            float m_TmpVector;
+
+            ModelManager.Instance.GetCurrentModel().transform.Rotate(Vector3.back, m_Controller.GetTouchpadAxis().x * 40 * Time.deltaTime);
+
+            /*Vector2 m_TmpVector;
+
+            Debug.Log(m_XVector);
 
             if (m_Controller.GetTouchpadAxis().x < -0.01 || m_Controller.GetTouchpadAxis().x > 0.01)
             {
-                m_TmpVector = m_Controller.GetTouchpadAxis().x - m_PrevVector.x;
-            } else
+                m_TmpVector.x = m_Controller.GetTouchpadAxis().x - m_PrevVector.x;
+            }
+            else
             {
-                m_TmpVector = m_PrevVector.x;
+                m_TmpVector.x = m_PrevVector.x;
             }
 
-            if (m_TmpVector < -0.01)
+            Mathf.Clamp(m_TmpVector.x, -1, 1);
+
+            if (m_TmpVector.x < -0.01)
             {
-                m_XVector -= Mathf.Abs(m_TmpVector);
+                m_XVector -= Mathf.Abs(m_TmpVector.x * 5);
+                if (m_XVector < 0)
+                {
+                    m_XVector += 360;
+                }
                 Debug.Log("Vector (Negative): " + m_XVector);
                 m_TargetRotation *= Quaternion.AngleAxis(m_XVector * 5, Vector3.back);
                 ModelManager.Instance.GetCurrentModel().transform.rotation = Quaternion.Lerp(
                     ModelManager.Instance.GetCurrentModel().transform.rotation,
-                    m_TargetRotation, 25 * m_RotationSmoothing * Time.deltaTime
+                    m_TargetRotation, 5 * m_RotationSmoothing * Time.deltaTime
                 );
-                Debug.Log("rotated");
             }
-            else if (m_TmpVector > 0.01)
+            else if (m_TmpVector.x > 0.01)
             {
-                m_XVector += Mathf.Abs(m_TmpVector);
+                m_XVector += Mathf.Abs(m_TmpVector.x * 5);
+                if (m_XVector > 360)
+                {
+                    m_XVector -= 360;
+                }
                 Debug.Log("Vector (Positive): " + m_XVector);
                 m_TargetRotation *= Quaternion.AngleAxis(m_XVector * 5, Vector3.back);
                 ModelManager.Instance.GetCurrentModel().transform.rotation = Quaternion.Lerp(
                     ModelManager.Instance.GetCurrentModel().transform.rotation,
-                    m_TargetRotation, 25 * m_RotationSmoothing * Time.deltaTime
+                    m_TargetRotation, 5 * m_RotationSmoothing * Time.deltaTime
                 );
-                Debug.Log("rotated");
             }
+            //}
             
-            m_TmpVector = m_Controller.GetTouchpadAxis().y - m_PrevVector.y;
+            /*if (m_Controller.GetTouchpadAxis().x < 0.1 && m_Controller.GetTouchpadAxis().x > -0.1)
+            {
+                if (m_Controller.GetTouchpadAxis().y < -0.01 || m_Controller.GetTouchpadAxis().y > 0.01)
+                {
+                    m_TmpVector.y = m_Controller.GetTouchpadAxis().y - m_PrevVector.y;
+                }
+                else
+                {
+                    m_TmpVector.y = m_PrevVector.y;
+                }
 
-            /*if (m_TmpVector < -0.01)
-            {
-                m_YVector -= Mathf.Abs(m_TmpVector);
-                Debug.Log("Vector (Negative): " + m_XVector);
-                m_TargetRotation *= Quaternion.AngleAxis(m_YVector, Vector3.up);
-                ModelManager.Instance.GetCurrentModel().transform.rotation = Quaternion.Lerp(
-                    ModelManager.Instance.GetCurrentModel().transform.rotation,
-                    m_TargetRotation, 25 * m_RotationSmoothing * Time.deltaTime
-                );
-                Debug.Log("rotated");
-            }
-            else if (m_TmpVector > 0.01)
-            {
-                m_YVector += Mathf.Abs(m_TmpVector);
-                Debug.Log("Vector (Positive): " + m_YVector);
-                m_TargetRotation *= Quaternion.AngleAxis(m_YVector, Vector3.up);
-                ModelManager.Instance.GetCurrentModel().transform.rotation = Quaternion.Lerp(
-                    ModelManager.Instance.GetCurrentModel().transform.rotation,
-                    m_TargetRotation, 25 * m_RotationSmoothing * Time.deltaTime
-                );
-                Debug.Log("rotated");
+                if (m_TmpVector.y < -0.01)
+                {
+                    if (m_YVector < -359)
+                    {
+                        m_YVector = 360;
+                    }
+                    m_YVector -= Mathf.Abs(m_TmpVector.y);
+                    Debug.Log("Vector (Negative): " + m_XVector);
+                    m_TargetRotation *= Quaternion.AngleAxis(m_YVector * 5, Vector3.up);
+                    ModelManager.Instance.GetCurrentModel().transform.rotation = Quaternion.Lerp(
+                        ModelManager.Instance.GetCurrentModel().transform.rotation,
+                        m_TargetRotation, 5 * m_RotationSmoothing * Time.deltaTime
+                    );
+                }
+                else if (m_TmpVector.y > 0.01)
+                {
+                    if (m_YVector > 359)
+                    {
+                        m_YVector = -360;
+                    }
+                    m_YVector += Mathf.Abs(m_TmpVector.y);
+                    Debug.Log("Vector (Positive): " + m_XVector);
+                    m_TargetRotation *= Quaternion.AngleAxis(m_YVector * 5, Vector3.up);
+                    ModelManager.Instance.GetCurrentModel().transform.rotation = Quaternion.Lerp(
+                        ModelManager.Instance.GetCurrentModel().transform.rotation,
+                        m_TargetRotation, 25 * m_RotationSmoothing * Time.deltaTime
+                    );
+                }
             }*/
 
-            m_PrevVector = m_Controller.GetTouchpadAxis();
+            //m_PrevVector = m_Controller.GetTouchpadAxis();
         }
     }
 }
