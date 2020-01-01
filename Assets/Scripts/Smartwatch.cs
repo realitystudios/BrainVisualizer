b5cac0e8ffce31728c39db769ade90b029405f44
Thomas Day <tday42@gmail.com>
2020-01-01 23:17:41 +0000

    Added time to watch

---
+++ Assets/Scripts/Smartwatch.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Smartwatch : MonoBehaviour
+{
+    [Header("DateTime Elements")]
+    [SerializeField]
+    private TextMeshProUGUI time;
+    [SerializeField]
+    private TextMeshProUGUI date;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time.SetText(
+            DateTime.Now.ToString("HH:mm")
+        );
+
+        date.SetText(
+            DateTime.Now.ToString("ddd d MMM")
+        );
+    }
+}
