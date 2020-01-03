f346fa6c58b8f9317fcf21fea19ce31181eec492
Thomas Day <tday42@gmail.com>
2020-01-03 17:12:04 +0000

    Updated Main Scene

--- Assets/Scripts/Smartwatch.cs
+++ Assets/Scripts/Smartwatch.cs
@@ -9,24 +9,37 @@ public class Smartwatch : MonoBehaviour
     [Header("DateTime Elements")]
     [SerializeField]
     private TextMeshProUGUI time;
-    [SerializeField]
-    private TextMeshProUGUI date;
+
+    private Coroutine m_TimeCoroutine;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        time.richText = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        time.SetText(
-            DateTime.Now.ToString("HH:mm")
-        );
+    }
 
-        date.SetText(
-            DateTime.Now.ToString("ddd d MMM")
-        );
+    private void OnEnable()
+    {
+        m_TimeCoroutine = StartCoroutine(StartWatch());
+    }
+
+    private IEnumerator StartWatch()
+    {
+        for(; ; )
+        {
+            time.text = DateTime.Now.ToString("HH:mm") + "<br> <size=0.5>" + DateTime.Now.ToString("ddd d MMM") + "</size>";
+
+            yield return new WaitForSecondsRealtime(60);
+        }
+    }
+
+    private void OnDisable()
+    {
+        StopCoroutine(m_TimeCoroutine);
     }
 }
