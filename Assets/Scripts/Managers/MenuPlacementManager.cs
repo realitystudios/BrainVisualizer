93b3159776518f5c51c5cfad4779e8ed1bfed1dd
Thomas Day <tday42@gmail.com>
2020-03-30 13:28:58 +0100

    Added device check

--- Assets/Scripts/Managers/MenuPlacementManager.cs
+++ Assets/Scripts/Managers/MenuPlacementManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using VRTK;
 
 public class MenuPlacementManager : MonoBehaviour
 {
@@ -10,23 +11,44 @@ public class MenuPlacementManager : MonoBehaviour
     private Transform m_RightWristPosition;
     [SerializeField]
     private Transform m_MenuPosition;
-
+    [SerializeField]
+    private float m_MenuSize = 0.4f;
     [SerializeField]
     private GameObject m_WatchPrefab;
     [SerializeField]
     private GameObject m_MenuPrefab;
 
+    private GameObject m_MenuObject;
+
     // Start is called before the first frame update
     void Start()
     {
-        GameObject watch = Instantiate(m_WatchPrefab, m_RightWristPosition);
-
         StartCoroutine(GetControllerType());
     }
 
     private IEnumerator GetControllerType()
     {
-        yield return new WaitUntil(() => OVRInput.GetConnectedControllers() != OVRInput.Controller.None);
-        Debug.Log(OVRInput.GetConnectedControllers());
+        yield return new WaitUntil(() => OVRPlugin.GetSystemHeadsetType() != OVRPlugin.SystemHeadset.None);
+        yield return new WaitUntil(() => FindObjectsOfType<VRTK_Pointer>().Length > 0);
+        VRTK.VRTK_Pointer[] pointers = FindObjectsOfType<VRTK.VRTK_Pointer>();
+
+        switch (OVRPlugin.GetSystemHeadsetType())
+        {
+            case OVRPlugin.SystemHeadset.Oculus_Go:
+                m_MenuObject = Instantiate(m_MenuPrefab, m_MenuPosition);
+                m_MenuObject.transform.localScale.Set(m_MenuSize, m_MenuSize, m_MenuSize);
+                foreach(var pointer in pointers)
+                {
+                    pointer.interactWithObjects = true;
+                    pointer.grabToPointerTip = true;
+                }
+                break;
+            case OVRPlugin.SystemHeadset.Rift_S:
+            case OVRPlugin.SystemHeadset.Oculus_Quest:
+                m_MenuObject = Instantiate(m_WatchPrefab, m_RightWristPosition);
+                break;
+            default:
+                break;
+        }
     }
 }
